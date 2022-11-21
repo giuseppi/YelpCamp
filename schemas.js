7f9605be3e7e8798a6305db118acd54f0915b5a9
Giuseppi Pelayo <giuseppipelayo@gmail.com>
2022-11-20 19:05:14 -0800

    fixed few schema errors

--- schemas.js
+++ schemas.js
@@ -1,12 +1,12 @@
 const Joi = require('joi');
 
-cmodule.exports.campgroundSchema = Joi.object({
+module.exports.campgroundSchema = Joi.object({
     campground : Joi.object({
-        title : Joi.string.required(),
-        price : Joi.number.required().min(0),
-        image : Joi.string.required(),
-        location : Joi.string.required(),
-        description : Joi.string.required()
+        title : Joi.string().required(),
+        price : Joi.number().required().min(0),
+        image : Joi.string().required(),
+        location : Joi.string().required(),
+        description : Joi.string().required()
     }).required
 });
 
